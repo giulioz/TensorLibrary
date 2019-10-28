d0c6ac1212be3ed13cab6ff62c7cb5e93d761737
Giulio Zausa <mail.zausa.giulio@gmail.com>
2019-10-28 22:27:31 +0100

    Catch2 Test integration

---
+++ test.cc
@@ -0,0 +1,8 @@
+// #define CATCH_CONFIG_MAIN
+#include <catch2/catch.hpp>
+
+#include "Tensor.hpp"
+
+// TEST_CASE("2: Factorial of 0 is 1 (fail)", "[multi-file:2]") {
+//   REQUIRE(Factorial(0) == 1);
+// }
