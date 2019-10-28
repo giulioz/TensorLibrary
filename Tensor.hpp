b2073093c18e63eec127ea38a408dc83edaeba37
Giulio Zausa <mail.zausa.giulio@gmail.com>
2019-10-28 22:27:05 +0100

    Iterator WIP
    Co-authored-by: Sandro Baccega <sandrobac@hotmail.it>

--- Tensor.hpp
+++ Tensor.hpp
@@ -5,46 +5,136 @@
 #include <functional>
 #include <initializer_list>
 #include <iostream>
+#include <iterator>
 #include <memory>
 #include <numeric>
 #include <tuple>
 #include <vector>
 
-using DimensionsList = const std::initializer_list<size_t> &;
+using DimensionsList = const std::initializer_list<size_t>&;
 
-static size_t calcDataSize(DimensionsList dimensions) {
+static inline size_t calcDataSize(DimensionsList dimensions) {
   return std::accumulate(std::begin(dimensions), std::end(dimensions), 1,
                          std::multiplies<double>());
 }
 
-static std::vector<size_t> calcStrides(DimensionsList dimensions) {
+static inline std::vector<size_t> calcStrides(DimensionsList dimensions) {
   std::vector<size_t> strides;
 
-  size_t lastStride = 1;
-  strides.emplace(strides.begin(), lastStride);
-  // strides.push_back(lastStride);
-
-  for (auto &&dim : dimensions) {
-    size_t stride = lastStride * dim;
-    strides.emplace(strides.begin(), stride);
-    lastStride = stride;
+  size_t stride = 1;
+  for (auto&& dim : dimensions) {
+    strides.push_back(stride);
+    stride *= dim;
   }
 
   return strides;
 }
 
 template <typename ValueType>
-class TensorIterator {
-  size_t currentPos;
-
- public:
-  TensorIterator(size_t startPos = 0) : currentPos(startPos) {}
-
-  void operator++() { currentPos++; }
-};
-
-template <typename ValueType>
 class Tensor {
+ public:
+  template <typename ValueTypeIter>
+  class TensorIterator
+      : public std::iterator<std::random_access_iterator_tag, ValueTypeIter> {
+    friend class Tensor;
+    Tensor* tensor;
+    size_t currentPos;
+
+    using pointer = typename std::iterator<std::random_access_iterator_tag,
+                                           ValueTypeIter>::pointer;
+    using reference = typename std::iterator<std::random_access_iterator_tag,
+                                             ValueTypeIter>::reference;
+    using difference_type =
+        typename std::iterator<std::random_access_iterator_tag,
+                               ValueTypeIter>::difference_type;
+
+    TensorIterator(Tensor *tensor, size_t startPos = 0)
+        : tensor(tensor), currentPos(startPos) {}
+
+   public:
+    TensorIterator& operator=(const TensorIterator<ValueTypeIter>& other) {
+      currentPos = other.currentPos;
+      return *this;
+    }
+
+    reference operator*() const { return (*tensor)[currentPos]; }
+
+    pointer operator->() const { return &((*tensor)[currentPos]); }
+
+    TensorIterator& operator++() {
+      ++currentPos;
+      return *this;
+    }
+
+    TensorIterator& operator--() {
+      --currentPos;
+      return *this;
+    }
+
+    TensorIterator operator++(int) {
+      return TensorIterator(tensor, currentPos++);
+    }
+
+    TensorIterator operator--(int) {
+      return TensorIterator(tensor, currentPos--);
+    }
+
+    TensorIterator operator+(const difference_type& n) const {
+      return TensorIterator(tensor, (currentPos + n));
+    }
+
+    TensorIterator& operator+=(const difference_type& n) {
+      currentPos += n;
+      return *this;
+    }
+
+    TensorIterator operator-(const difference_type& n) const {
+      return TensorIterator(tensor, (currentPos - n));
+    }
+
+    TensorIterator& operator-=(const difference_type& n) {
+      currentPos -= n;
+      return *this;
+    }
+
+    reference operator[](const difference_type& n) const {
+      return (*tensor)[currentPos + n];
+    }
+
+    bool operator==(const TensorIterator& other) const {
+      return currentPos == other.currentPos;
+    }
+
+    bool operator!=(const TensorIterator& other) const {
+      return currentPos != other.currentPos;
+    }
+
+    bool operator<(const TensorIterator& other) const {
+      return currentPos < other.currentPos;
+    }
+
+    bool operator>(const TensorIterator& other) const {
+      return currentPos > other.currentPos;
+    }
+
+    bool operator<=(const TensorIterator& other) const {
+      return currentPos <= other.currentPos;
+    }
+
+    bool operator>=(const TensorIterator& other) const {
+      return currentPos >= other.currentPos;
+    }
+
+    difference_type operator+(const TensorIterator& other) const {
+      return currentPos + other.currentPos;
+    }
+
+    difference_type operator-(const TensorIterator& other) const {
+      return currentPos - other.currentPos;
+    }
+  };
+
+ private:
   std::vector<ValueType> data;
   std::vector<size_t> strides;
   std::vector<size_t> sizes;
@@ -53,8 +143,7 @@ class Tensor {
   size_t coordsToIndex(DimensionsList coords) {
     size_t index = 0;
     size_t strideIndex = 0;
-    // size_t size = strides.size();
-    for (auto &&coord : coords) {
+    for (auto&& coord : coords) {
       index += coord * (strides.at(strideIndex));
       strideIndex++;
     }
@@ -63,6 +152,9 @@ class Tensor {
   }
 
  public:
+  using iterator = TensorIterator<ValueType>;
+  using const_iterator = TensorIterator<const ValueType>;
+
   Tensor(DimensionsList sizes) : data(calcDataSize(sizes)) {
     this->sizes = sizes;
     _totalItems = data.size();
@@ -73,34 +165,48 @@ class Tensor {
 
   size_t items(size_t dimension) const { return sizes.at(dimension); }
 
-  TensorIterator<ValueType> begin() {}
+  iterator begin() { return iterator(this); }
+  const_iterator begin() const { return const_iterator(this); }
+  const_iterator cbegin() const { return const_iterator(this); }
+  iterator end() { return iterator(this, _totalItems); }
+  const_iterator end() const { return const_iterator(this, _totalItems); }
+  const_iterator cend() const { return const_iterator(this, _totalItems); }
 
-  void initData(ValueType defaultValue) {
-    // std::fill(begin(), end(), defaultValue);
+  void fill(ValueType defaultValue) {
+    std::fill(begin(), end(), defaultValue);
   }
 
-  ValueType &operator[](DimensionsList coords) {
+  ValueType& operator[](int linearCoord) {
+    return data.at(linearCoord);
+  }
+
+  ValueType& operator[](DimensionsList coords) {
     auto index = coordsToIndex(coords);
     return data.at(index);
   }
 
   void printTensor() const {
+    // for (size_t i = 0; i < _totalItems;currentPosi++) {
+    //   for (size_t j = 0; j < sizes.size(); j++) {
+    //     if (i % sizes.size() == 0 && i != 0) {
+    //       std::cout << std::endl;
+    //     }
+    //   }
+    //   std::cout << data[i] << ", ";
+    // }
+    // std::cout << std::endl;
+
+    std::cout << std::endl << "Data" << std::endl;
     for (size_t i = 0; i < _totalItems; i++) {
-      for (size_t j = 0; j < sizes.size(); j++) {
-        if (i % sizes.size() == 0 && i != 0) {
-          std::cout << std::endl;
-        }
-      }
-      std::cout << data[i] << " ";
+      std::cout << data[i] << ", ";
     }
-    std::cout << std::endl;
-    for (size_t i = 0; i < _totalItems; i++) {
-      std::cout << data[i] << " ";
-    }
-    std::cout << std::endl;
+
+    std::cout << std::endl << "Strides" << std::endl;
     for (size_t i = 0; i < strides.size(); i++) {
       std::cout << strides[i] << " ";
     }
+
+    std::cout << std::endl;
   }
 };
 
--- main.cc
+++ main.cc
@@ -4,25 +4,41 @@
 
 int main() {
   Tensor<int> tensor({2, 4, 2});
-  tensor[{0, 0, 0}] = 1;
-  tensor[{0, 1, 0}] = 2;
-  tensor[{0, 2, 0}] = 3;
-  tensor[{0, 3, 0}] = 4;
-  tensor[{1, 0, 0}] = 1;
-  tensor[{1, 1, 0}] = 2;
-  tensor[{1, 2, 0}] = 3;
-  tensor[{1, 3, 0}] = 4;
-  tensor[{0, 0, 1}] = 11;
-  tensor[{0, 1, 1}] = 22;
-  tensor[{0, 2, 1}] = 33;
-  tensor[{0, 3, 1}] = 44;
-  tensor[{1, 0, 1}] = 11;
-  tensor[{1, 1, 1}] = 22;
-  tensor[{1, 2, 1}] = 33;
-  tensor[{1, 3, 1}] = 44;
-  // tensor[{0}] = 10;
 
-  // std::cout << tensor[{0}] << std::endl;
+  tensor[{0, 0, 0}] = 100;
+  tensor[{0, 1, 0}] = 200;
+  tensor[{0, 2, 0}] = 300;
+  tensor[{0, 3, 0}] = 400;
+  tensor[{1, 0, 0}] = 110;
+  tensor[{1, 1, 0}] = 210;
+  tensor[{1, 2, 0}] = 310;
+  tensor[{1, 3, 0}] = 410;
+  tensor[{0, 0, 1}] = 101;
+  tensor[{0, 1, 1}] = 201;
+  tensor[{0, 2, 1}] = 301;
+  tensor[{0, 3, 1}] = 401;
+  tensor[{1, 0, 1}] = 111;
+  tensor[{1, 1, 1}] = 211;
+  tensor[{1, 2, 1}] = 311;
+  tensor[{1, 3, 1}] = 411;
+  
+  auto iterator = tensor.begin();
+  iterator++;
+  std::cout << "Iteratore: " << *iterator << std::endl;
+
   tensor.printTensor();
+
+  std::cout << "0,3: " << tensor[{0, 3}] << std::endl;
+  std::cout << "1,2: " << tensor[{1, 2}] << std::endl;
+
+  // tensor.fill(666);
+  auto iteratorFill = tensor.begin();
+  while (iteratorFill < tensor.end()) {
+    *iteratorFill = 666;
+    iteratorFill++;
+  }
+  tensor.printTensor();
+
+  //for example a rank 3 tensor of size (3,4,5) represented in right-major order will have strides (20,5,1) and width (3,4,5). Entry (i,j,k) will be at index (20*i+5*j+k*1) in the flat storage.
   return 0;
 }
